29d779c3f78127bae045ff72f4119394fa62282c
Eugene-Afanasev <whoaeugene@gmail.com>
2020-06-30 01:00:09 +1000

    shortest way in process

---
+++ Appearance/Way.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AaDS_Project.Appearance
+{
+    public class Way
+    {
+        List<int> points;
+        public Way()
+        {
+        }
+    }
+}
