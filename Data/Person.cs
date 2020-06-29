b1409acca77317af2efe78ea9286daff3f04a797
dma117 <ysters14@gmail.com>
2020-06-29 23:20:28 +1000

    Add class PersonContainer and randomizer for making users

--- Data/Person.cs
+++ Data/Person.cs
@@ -1,6 +1,9 @@
 ﻿#region
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
 
 #endregion
 
@@ -8,7 +11,7 @@ namespace AaDS_Project.Data
 {
     public class Person
     {
-        private static int _count = 0;
+        private static int _count;
         private readonly int _id;
 
         public Person(string name, Dictionary<Time, string> schedule)
@@ -34,6 +37,50 @@ namespace AaDS_Project.Data
 
     public class PersonContainer
     {
-        // TODO
+        private readonly List<Person> _users;
+
+        public PersonContainer()
+        {
+            _users = new List<Person>();
+        }
+
+        public void GetUsers(int number)
+        {
+            var random = new Random();
+            
+            RandomizeUsers(number, random);
+        }
+
+        private void RandomizeUsers(int numberOfUsers, Random random)
+        {
+            for (int i = 0; i < numberOfUsers; i++)
+            {
+                var schedule = new Dictionary<Time, String>();
+                schedule[RandomizeTime(random)] = Places.Names[random.Next(Places.Names.Count)];
+                
+                var name = UsersNames.Names[random.Next(UsersNames.Names.Count)];
+                var person = new Person(name, schedule);
+                
+                _users.Add(person);
+            }
+        }
+
+        private static Time RandomizeTime(Random random)
+        {
+            int min = (int)Enum.GetValues(typeof(Time)).Cast<Time>().Min();
+            int max = (int)Enum.GetValues(typeof(Time)).Cast<Time>().Max();
+
+            return (Time)random.Next(min, max + 1);
+        }
+    }
+    
+    public static class UsersNames
+    {
+        public static readonly List<string> Names = new List<string>
+        {
+            "Misha", "Masha", "Sasha", "Zhenya", "Ruslan", "Nikita", "Lilit",
+            "Yu", "Anna", "Ilya", "Elena", "Andrey", "Vasilii", "Ivan", "Peter", 
+            "Tanya", "Olya", "Hui"
+        };
     }
 }
